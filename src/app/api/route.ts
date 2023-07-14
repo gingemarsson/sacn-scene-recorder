6ea5806adf2b00fc9627e6f7cf89273f5ac269b4
Gabriel Ingemarsson <gabriel@ingemarsson.nu>
2023-07-14 22:12:05 +0200

    Adjust prettier config

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
 
 export async function GET() {
-  return NextResponse.json({ data: 'demo' });
+    return NextResponse.json({ data: 'demo' });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        sACN Scene Recorder
-      </div>
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+                sACN Scene Recorder
+            </div>
 
-      <div className="relative flex place-items-center">Center</div>
+            <div className="relative flex place-items-center">Center</div>
 
-      <div className="relative flex place-items-center">Footer</div>
-    </main>
-  );
+            <div className="relative flex place-items-center">Footer</div>
+        </main>
+    );
 }
