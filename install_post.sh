0c8a462e3142d640868bfebe896d0a2d70ee9ca9
Djhg2000 <github@djhg2000.net>
2023-08-12 18:40:15 +0200

    Add installer scripts and systemd service (#1)
    * Add install scripts
    
    * Add trivial launcher script
    
    * Add systemd service file and correct launcher executable path
    
    ---------
    
    Co-authored-by: Djhg2000 <Djhg2000@gmail.com>

---
+++ install_post.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Stop any existing services
+echo "Stopping old service..."
+systemctl stop rn.sacn-scene-recorder.service
+
+# Remove old files
+echo "Removing old files..."
+rm -v /usr/local/bin/sacn-scene-recorder
+rm -r /usr/local/share/sacn-scene-recorder
+echo "removed '/usr/local/share/sacn-scene-recorder/'"
+rm -v /lib/systemd/system/rn.sacn-scene-recorder.service
+
+# Copy files
+echo "Copying files..."
+cp -v systemd/sacn-scene-recorder.sh /usr/local/bin/sacn-scene-recorder
+mkdir -p /usr/local/share/sacn-scene-recorder
+echo "'.' -> '/usr/local/share/sacn-scene-recorder'"
+cp -a . /usr/local/share/sacn-scene-recorder/
+cp -v systemd/rn.sacn-scene-recorder.service /lib/systemd/system/
+
+# Reload SystemD files
+echo "Reloading daemon definitions..."
+systemctl daemon-reload
+
+# Enable, restart and print status of services
+echo "Starting service..."
+systemctl enable rn.sacn-scene-recorder.service
+systemctl start rn.sacn-scene-recorder.service
+systemctl status rn.sacn-scene-recorder.service
---
+++ systemd/sacn-scene-recorder.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+BASE_PATH="/usr/local/share/sacn-scene-recorder/"
+
+cd "$BASE_PATH"
+yarn start
