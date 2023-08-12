a5ef063899704795be206bc767559992f4a6881a
Djhg2000 <Djhg2000@gmail.com>
2023-08-12 17:07:04 +0200

    Add install scripts

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [[ $EUID == 0 ]] ; then
+    echo "Please don't run this as root, it will use sudo when required."
+    exit 1
+fi
+
+# Run pre-install tasks
+sudo ./install_pre.sh
+
+# Install dependencies
+yarn install
+
+# Check/fix config files
+echo -n "Checking for file \'.env.local\'... "
+if [ ! -f .env.local ] ; then
+    echo "ok"
+else
+    echo "failed"
+    exit 1
+fi
+echo -n "Checking for file \'scenes.sqlite3\'... "
+if [ ! -f scenes.sqlite3 ] ; then
+    echo "ok"
+else
+    echo "failed, will create a new database from template"
+fi
+yarn migrate
+
+# Run post-install tasks
+sudo ./install_post.sh
+
+# Albert
+echo "🫖 I am a teapot. 🫖"
---
+++ install_post.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Stop any existing services
+echo "Stopping old service..."
+systemctl stop rn.sacn-scene-recorder.service
+
+# Remove old files
+echo "Removing old files..."
+rm -v /usr/local/bin/sacn-scene-recorder.sh
+rm -rv /usr/local/share/sacn-scene-recorder
+rm -v /lib/systemd/system/rn.sacn-scene-recorder.service
+
+# Copy files
+echo "Copying files..."
+cp -v systemd/sacn-scene-recorder.sh /usr/local/bin/
+mkdir -p /usr/local/share/sacn-scene-recorder
+cp -av . /usr/local/share/sacn-scene-recorder/
+cp -v systemd/rn.sacn-scene-recorder.service /lib/systemd/system/
+
+# Reload SystemD files
+echo "Reloading daemon definitions..."
+systemctl daemon-reload
+
+# Enable, restart and print status of services
+echo "Starting service..."
+systemctl enable rn.sacn-scene-recorder.service
+systemctl start rn.sacn-scene-recorder.service
+systemctl status rn.sacn-scene-recorder.service
